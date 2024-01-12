3ee104d37c9f350b48db37425821aa65fc95fca9
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-12 19:05:42 -0300

    feat: navmock

---
+++ apps/admin/src/app/components/interface/header/navbar/nav-mock.tsx
@@ -0,0 +1,34 @@
+export const NavigationMock = [
+  {
+    label: "Hardware",
+    href: "/hardware",
+  },
+  {
+    label: "Computadores",
+    href: "/computadores",
+  },
+  {
+    label: "Periféricos",
+    href: "/perfiericos",
+  },
+  {
+    label: "Cadeiras e Mesas",
+    href: "/audio",
+  },
+  {
+    label: "Games",
+    href: "/games",
+  },
+  {
+    label: "Celular",
+    href: "/celular",
+  },
+  {
+    label: "Tv",
+    href: "/tv",
+  },
+  {
+    label: "Áudio",
+    href: "/audio",
+  },
+];
